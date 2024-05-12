2ee68276ef6793f442fcd2cb2b2126675651ed7e
nominalrune <nominalrune.yokoyama@gmail.com>
2024-05-12 12:55:34 +0900

    button

--- spa/app/components/common/Button/CircleButton.tsx
+++ spa/app/components/common/Button/CircleButton.tsx
@@ -1,14 +1,14 @@
 import { twMerge } from 'tailwind-merge';
 interface CircleButtonProps {
-	  children: React.ReactNode;
-  onClick: () => void;
-  className: string;
+	children: React.ReactNode;
+	onClick: () => void;
+	className: string;
 }
 
-export default function CircleButton({ children, onClick, className="size-5", ...props }: CircleButtonProps) {
-  return (
-	<button onClick={onClick} className={twMerge("rounded-full flex justify-center items-center",className)} {...props}>
-	  {children}
-	</button>
-  );
+export default function CircleButton({ children, onClick, className = "size-5", ...props }: CircleButtonProps) {
+	return (
+		<button onClick={onClick} className={twMerge("rounded-full flex justify-center items-center bg-blue-600 hover:bg-blue-500 text-white", className)} {...props}>
+			{children}
+		</button>
+	);
 }
